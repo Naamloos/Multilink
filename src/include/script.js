4e01b4a36e5ed3d3c66a9ba0f5c44e6b9786b78c
Thomas May <wamwoowam@gmail.com>
2019-12-15 22:50:18 +0000

    Remove dependency on JQuery and use ES6

--- src/include/script.js
+++ src/include/script.js
@@ -1,41 +1,51 @@
 // Loading links.json
-$.getJSON("./include/links.json", function(data)
-{
-    $("#header").append(data.header);
-    // iterating through loaded links
-    data.links.forEach(link => 
-    {
-        // Creating a clickable anchor with target set to new tab
-        var anchor = document.createElement("a");
-        anchor.href = link.url;
-        anchor.target = "_blank";
+fetch("./include/links.json")
+    .then(resp => resp.json())
+    .then(data => {
+        let header = document.getElementById("header");
+        let content = document.getElementById("content");
+        
+        header.append(data.header);
 
-        // constructing the box model shit
-        var itembox = document.createElement("div");
-        var itemimgbox = document.createElement("div");
-        var itemcontent = document.createElement("div");
-        var itemimg = document.createElement("img");
+        for (const link of data.links) {
+            // Creating a clickable anchor with target set to new tab
+            let anchor = document.createElement("a");
+            anchor.href = link.url;
+            anchor.target = "_blank";
 
-        // Setting classes
-        itembox.className = "linkbox";
-        itemcontent.className = "linkcontent";
-        itemimgbox.className = "linkimgbox";
+            // constructing the box model shit
+            let itembox = document.createElement("div");
+            let itemimgbox = document.createElement("div");
+            let itemcontent = document.createElement("div");
+            let itemimg = document.createElement("img");
+            let itemtext = document.createTextNode(link.name);
+            let itemsmalllink = document.createElement("div");
 
-        // Adding text to a link
-        itemcontent.innerHTML = link.name + "<div class='smalllinktext'>" + link.url + "</div>";
+            // Setting classes
+            itembox.className = "linkbox";
+            itemcontent.className = "linkcontent";
+            itemimgbox.className = "linkimgbox";
+            itemsmalllink.className = "smalllinktext";
 
-        // filling the link image
-        itemimg.className = "linkimg";
-        itemimg.src = link.image;
-        itemimg.alt = link.name;
+            // Set the inner link text
+            itemsmalllink.innerText = link.url;
 
-        // Appending everything to each other
-        itemimgbox.append(itemimg);
-        itembox.append(itemimgbox);
-        itembox.append(itemcontent);
-        anchor.append(itembox);
+            // Adding text to a link
+            itemcontent.appendChild(itemtext);
+            itemcontent.appendChild(itemsmalllink);
 
-        // finally, appending to the master div
-        $("#content").append(anchor);
+            // filling the link image
+            itemimg.className = "linkimg";
+            itemimg.src = link.image;
+            itemimg.alt = link.name;
+
+            // Appending everything to each other
+            itemimgbox.append(itemimg);
+            itembox.append(itemimgbox);
+            itembox.append(itemcontent);
+            anchor.append(itembox);
+
+            // finally, appending to the master div
+            content.appendChild(anchor);
+        }
     });
-});
